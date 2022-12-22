3187384627af83a130301b7a854b57a8296ec360
Ivan M <slade91@mail.ru>
2022-12-22 19:17:54 +0300

    added Tasks

---
+++ Task[1].py
@@ -0,0 +1,21 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def PredicCheck():
+    
+    print('Программа для проверки истинности утверждения')
+    print('¬(X V Y V Z) = ¬X ⋀ ¬Y ⋀ ¬Z', end=' ')
+    print('для всех значений предикат.')
+    print()
+    print('X','Y','Z' )
+    
+    for x in [0, 1]:
+        for y in [0, 1]:
+            for z in [0, 1]:
+                print(x, y, z)
+                p1 = not (x or y or z)
+                p2 = not x and not y and not z
+                print(p1 == p2)
+    return PredicCheck
+    
+
+PredicCheck()
